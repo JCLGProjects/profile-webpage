3bc4d352782efd423a619ee371bac8173203a35e
Camilo López <camilino123@gmail.com>
2019-05-22 22:19:58 -0500

    Some functions initialized

--- src/App.js
+++ src/App.js
@@ -4,6 +4,31 @@ import BottomNavigationNoLabel from "./component/BottomNavigationNoLabel";
 import MiniVariantDrawer from "./component/MiniVariantDrawer";
 
 function App() {
+
+    function certifications() {
+
+    }
+
+    function main() {
+
+    }
+
+    function projects() {
+
+    }
+
+    function studies() {
+
+    }
+
+    function jobs() {
+
+    }
+
+    function contact() {
+
+    }
+
     return (
         <>
             <MiniVariantDrawer/>
